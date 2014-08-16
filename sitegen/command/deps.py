00cf895201b9ed81def2d2823e27c15b57c0c5f0
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2014-08-16 12:04:52 +0200

    siteloader: using new observer & action-based model
    Using the observers and actions for dependency collecting, but only pages
    and assets are supported, as of now themes (and blog posts) are ignored.

--- sitegen/command/deps.py
+++ sitegen/command/deps.py
@@ -1,5 +1,6 @@
 from sitegen.command.command import Command
-from sitegen.siteloader.loader import SiteLoader
+from sitegen.siteloader.loader import SiteLoader, MarkdownObserver, CopyObserver, DependencyObserver, \
+    DependencyCollector, FILE_TYPE_PAGE, FILE_TYPE_ASSET
 
 
 class Deps(Command):
@@ -11,15 +12,31 @@ class Deps(Command):
                             help='Site Root')
 
     def _perform(self) -> int:
-        loader = SiteLoader(self._ns.root[0])
+        loader = self.__create_site_loader()
         loader.update()
 
         print('\nDependencies:')
-        for d, v in loader.dependencies.items():
+        for d, v in self._dependency_collector.dependencies.items():
             print('  ', d + ':', v)
 
         print('\nActions')
-        for a, v in loader.actions.items():
+        actions = self._markdown_observer.actions
+        actions.update(self._asset_observer.actions)
+        for a, v in actions.items():
             print('  ', a + ':', v)
 
         return 0
+
+    def __create_site_loader(self) -> SiteLoader:
+        root = self._ns.root[0]
+        self._markdown_observer = MarkdownObserver(root)
+        self._asset_observer = CopyObserver(root)
+        self._dependency_collector = DependencyCollector()
+
+        self._page_deps_observer = DependencyObserver(self._dependency_collector, [self._markdown_observer], root)
+        self._asset_deps_observer = DependencyObserver(self._dependency_collector, [self._asset_observer], root)
+
+        loader = SiteLoader(root)
+        loader.register(FILE_TYPE_PAGE, self._page_deps_observer)
+        loader.register(FILE_TYPE_ASSET, self._asset_deps_observer)
+        return loader
--- sitegen/siteloader/loader.py
+++ sitegen/siteloader/loader.py
@@ -1,14 +1,25 @@
+from collections.abc import Sequence
 import os
 import shutil
 import markdown
 from sitegen.templates import File
 
+# Site's file types
+FILE_TYPE_PAGE = 'page'
+FILE_TYPE_BLOG = 'blog-post'
+FILE_TYPE_ASSET = 'asset-copy-as-is'
+FILE_TYPE_THEME = 'theme'
+
 
 class Action:
-    def __init__(self, path, target_path, **kwargs):
+    def __init__(self, path: str, target_path: str, site_root: str, **kwargs):
         self.path = path
         self.target_path = target_path
+        self.site_root = site_root
         self.kwargs = kwargs
+
+    def __str__(self):
+        return "{}('{}', '{}')".format(self.__class__.__name__, self.path, self.target_path)
 
     def run(self):
         pass
@@ -33,7 +44,7 @@ class FinalHtmlAction(Action):
             input_text = f.read()
         self.__render(template_dir, input_text, root)
 
-    def __get_root_dir(self, root):
+    def __get_root_dir(self, root: str) -> str:
         sub_path = self.target_path[len(root):].lstrip(os.sep)
         count = len(sub_path.split(os.sep)) - 1
         root_dir = os.curdir if count == 0 else os.pardir + (os.sep + os.pardir) * (count - 1)
@@ -51,47 +62,116 @@ class FinalHtmlAction(Action):
         file.update()
 
 
-
 class DependencyCollector:
-
     def __init__(self):
-        self.dependencies = dict(__site=list())
+        self._dependencies = dict(__site=list())
 
-    def add_site_dependency(self, path):
+    @property
+    def dependencies(self) -> dict:
+        return dict(self._dependencies)
+
+    def add_site_dependency(self, path: str):
         self.add_dependency('__site__', path)
 
-    def add_dependency(self, key, path):
-        if not key in self.dependencies:
-            self.dependencies[key] = list()
+    def add_dependency(self, key: str, path: str) -> None:
+        if not key in self._dependencies:
+            self._dependencies[key] = list()
 
-        self.dependencies[key].append(path)
+        self._dependencies[key].append(path)
 
 
 class FileSystemObserver:
-    def notify(self, directory, entry):
+    def __init__(self, site_root: str):
+        self._site_root = site_root
+
+    def notify(self, directory: str, entry: str):
         pass
 
 
 class ActionObserver(FileSystemObserver):
-    def __init__(self):
-        self.actions = dict()
+    def __init__(self, site_root: str):
+        super().__init__(site_root)
+        self._actions = dict()
 
+    def _add_action(self, path: str, action: Action):
+        self._actions[path] = action
 
-    def _add_action(self, path, action):
-        self.actions[path] = action
+    @property
+    def actions(self) -> dict:
+        return dict(self._actions)
 
 
 class CopyObserver(ActionObserver):
-    def notify(self, directory, entry):
+    def notify(self, directory: str, entry: str):
         full_path = os.path.join(directory, entry)
-        self._add_action(full_path, CopyAction(full_path, full_path, os.path.join('_install', full_path)))
+        self._add_action(full_path, CopyAction(full_path, os.path.join('_install', full_path), self._site_root))
 
 
 class MarkdownObserver(ActionObserver):
-    def notify(self, directory, entry):
+    def notify(self, directory: str, entry: str):
         if entry.endswith('.md'):
-            full_path = os.path.join(directory, entry)
-            self._add_action(full_path, MarkdownAction(full_path, os.path.join('_build', full_path[:-3] + '.middle')))
+            path = os.path.join(directory, entry)
+            sub_path_items = path.split(os.path.sep)[1:]
+            build_target_path = os.sep.join(['_build'] + sub_path_items)[:-3] + '.middle'
+            install_target_path = os.sep.join(['_install'] + sub_path_items)[:-3] + '.html'
+
+            self._add_action(path, MarkdownAction(path, build_target_path, self._site_root))
+            self._add_action(build_target_path,
+                             FinalHtmlAction(build_target_path, install_target_path, self._site_root))
+
+
+class DependencyObserver(FileSystemObserver):
+    def __init__(self, dependency_collector: DependencyCollector, observers: Sequence, site_root: str):
+        super().__init__(site_root)
+
+        self.dependency_collector = dependency_collector
+        self.observers = observers
+
+    def notify(self, directory: str, entry: str):
+        path = os.path.join(directory, entry)
+        self.dependency_collector.add_site_dependency(path)
+        for observer in self.observers:
+            observer.notify(directory, entry)
+
+
+class SiteLoader:
+    def __init__(self, site_root):
+        self._site_root = site_root
+        self._observers = dict()
+
+    def register(self, entry_type: str, observer: FileSystemObserver):
+        if not entry_type in self._observers:
+            self._observers[entry_type] = list()
+
+        self._observers[entry_type].append(observer)
+
+    def update(self):
+        entries = os.listdir(self._site_root)
+        for entry in entries:
+            if entry.startswith('_') or entry.startswith('.'):
+                continue
+
+            self._process_root_entry(entry)
+
+    def _process_root_entry(self, entry: str):
+        full_path = os.path.join(self._site_root, entry)
+        if entry == 'source' or entry == 'pages':
+            self.__process_path(full_path, FILE_TYPE_PAGE)
+        elif entry == 'posts':
+            pass
+        #    self.__process_blog_entries(path)
+        elif entry == 'templates':
+            pass
+        #    self.__process_theme_entries(os.path.join(entry, 'current'))
+        elif os.path.isdir(full_path):
+            self.__process_path(full_path, FILE_TYPE_ASSET)
+
+    def __process_path(self, path: str, observer_type: str):
+        for root, dirs, files in os.walk(path):
+            directory = root[len(self._site_root):].lstrip(os.sep)
+            for entry in files:
+                for observer in self._observers[observer_type]:
+                    observer.notify(directory, entry)
 
 
 class Loader:
@@ -141,7 +221,7 @@ class Loader:
         pass
 
 
-class SiteLoader(Loader):
+class XSiteLoader(Loader):
     """
     Loads the dependency graph based on the current state of the file system
     """
