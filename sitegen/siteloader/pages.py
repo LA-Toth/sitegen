4e84694da1fd1d1e973b1c3b165b45722344377b
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2014-08-23 14:25:53 +0200

    pages: support HTML pages next to markdown

--- sitegen/siteloader/pages.py
+++ sitegen/siteloader/pages.py
@@ -8,21 +8,30 @@ from sitegen.siteloader.dependency import Action
 
 class MarkdownObserver(FSDependencyObserver):
     def notify(self, directory: str, entry: str):
-        if entry.endswith('.md'):
+        is_md = entry.endswith('.md')
+        is_html = entry.endswith('.html')
+        if is_md or is_html:
+            name = os.path.splitext(entry)[0]
+
             path = os.path.join(directory, entry)
-            sub_path_items = path.split(os.path.sep)[1:]
-            build_target_path = os.sep.join(['_build'] + sub_path_items)[:-3] + '.middle'
+            name_path = os.path.join(directory, name)
+
+            sub_path_items = name_path.split(os.path.sep)[1:]
+
+            build_target_path = os.sep.join(['_build'] + sub_path_items) + '.middle'
             yaml_target_path = build_target_path + '.yml'
-            install_target_path = os.sep.join(['_install'] + sub_path_items)[:-3] + '.html'
+            install_target_path = os.sep.join(['_install'] + sub_path_items) + '.html'
+
+            action_class = MarkdownAction if is_md else HtmlAction
 
             self._dependency_collector.add_site_dependency([install_target_path])
             self._dependency_collector.add_dependency(install_target_path,
                                                       [build_target_path, yaml_target_path],
                                                       FinalHtmlAction)
-            self._dependency_collector.add_dependency(build_target_path, [path], MarkdownAction)
+            self._dependency_collector.add_dependency(build_target_path, [path], action_class)
 
 
-class MarkdownAction(Action):
+class _PageAction(Action):
     max_deps_count = 1
 
     def __get_input_text(self, path: str):
@@ -50,7 +59,7 @@ class MarkdownAction(Action):
 
         input_text, yaml_text = self.__get_input_text(path)
 
-        output_text = markdown.markdown(input_text, output_format='html5')
+        output_text = self._format_text(input_text)
 
         self.__write_output_files(output_text, target_path, yaml_target_path, yaml_text)
 
@@ -60,8 +69,21 @@ class MarkdownAction(Action):
         yaml_target_path = target_path + '.yml'
         return path, target_path, yaml_target_path
 
+    def _format_text(self, input_text: str):
+        raise NotImplementedError("Cannot generate output text")
+
     def __write_output_files(self, output_text, target_path, yaml_target_path, yaml_text):
         with open(target_path, 'wt') as f:
             f.write(output_text)
         with open(yaml_target_path, 'wt') as f:
             f.write(yaml_text)
+
+
+class MarkdownAction(_PageAction):
+    def _format_text(self, input_text: str):
+        return markdown.markdown(input_text, output_format='html5')
+
+
+class HtmlAction(_PageAction):
+    def _format_text(self, input_text: str):
+        return input_text
